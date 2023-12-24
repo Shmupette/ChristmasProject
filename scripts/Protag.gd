ca3ff0de24cd6369f033f3706658af61f058cb8f
Shmupette <joe.ashton@hotmail.co.uk>
2023-12-24 21:00:11 +0000

    update

--- scripts/Protag.gd
+++ scripts/Protag.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+func ready():
+	globalScript.set("player", self)
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
+	globalScript.set("player", self)
 	move_and_slide()
+
