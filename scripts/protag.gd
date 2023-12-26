a80b3bdea49d85b53b3124268d97333e47df64a8
codemiite <sewellray11+git@gmail.com>
2023-12-26 22:35:57 +0000

    added weapon, changed collision behaviour

--- scripts/protag.gd
+++ scripts/protag.gd
@@ -5,23 +5,31 @@ extends CharacterBody2D
 
 const HUD = preload("res://scenes/HUD.tscn")
 var hud = null
+const WEAPON = preload("res://scenes/weapon.tscn")
+@onready var weapon_pivot = $WeaponPivot
 
 func _ready():
 	hud = HUD.instantiate()
 	hud.setPlayer(self)
 	add_child(hud)
-
-
+	giveWeapon()
+	
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	weapon_pivot.rotate(0.05)
 
 func _input(event):
 	if event.is_action_pressed("damageTest"):
 		health -= 1
 		print("HEALTH:",health)
 
+func giveWeapon():
+	var weapon = WEAPON.instantiate()
+	weapon.setPlayer(self)
+	weapon.position = Vector2(0, 40)
+	weapon_pivot.add_child(weapon)
